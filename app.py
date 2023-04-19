6327856f4bc208478825dcbdb2242e3ef451d916
Dipendrakumar1 <dipendrayadav299@gmail.com>
2023-04-19 22:29:53 +0530

    Updated all files

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-### Integrate HTML With Flask
-### HTTP verb GET And POST
+from flask import Flask, redirect, url_for, render_template, request, jsonify
+import pickle
+import numpy as np
+app = Flask(__name__, template_folder='template')
+model = pickle.load(open('model.pkl', 'rb'))
 
-##Jinja2 template engine
-'''
-{%...%} conditions,for loops
-{{    }} expressions to print output
-{#....#} this is for comments
-'''
-from flask import Flask,redirect,url_for,render_template,request
-app=Flask(__name__)
+
 @app.route('/')
 def welcome():
     return render_template('index.html')
 
-if __name__=='__main__':
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    if prediction[0] == 1:
+        return render_template('index.html', prediction_text='The person is diabetic.')
+    else:
+        return render_template('index.html', prediction_text='The person is Non-diabetic.')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
---
+++ request.py
@@ -0,0 +1,6 @@
+# import requests
+
+# url = 'http://localhost:5000/predict_api'
+# r = requests.post(url, json={'Preganancies': 6, 'Glucose': 148, 'BloodPressure': 72, 'skinThickness': 35, 'Insulin': 0, 'BMI': 33, 'DiabetesPedigreeFunction':0 , 'Age':50 })
+
+# print(r.json())
